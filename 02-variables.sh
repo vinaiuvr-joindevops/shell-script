dd23ae9aaf3fd41bec9082b203b11df38c4a743c
vinaiuvr-joindevops <uvr.vinaikumar@gmail.com>
2024-12-30 16:55:54 +0530

    conversation added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-echo "Rahim: Hi rober"
-echo "Robert: Hi Rahim"
-echo " Rahim: How are you doing?"
-echo "Robert: I am good how are you?"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hi $PERSON1"
+echo " $PERSON1: How are you doing?"
+echo "$PERSON2: I am good how are you?"
