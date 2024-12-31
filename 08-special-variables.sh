65f1a5cd679111289011b3c8672cfc8bb01af120
vinaiuvr-joindevops <uvr.vinaikumar@gmail.com>
2024-12-31 17:08:54 +0530

    new file

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -7,4 +7,7 @@ echo "Present working directory: $PWD"
 echo "Home directory of current user:  $HOME"
 echo "Which user is running the script:  $USER"
 echo "Process Id of current script :  $$ "
+
+sleep 60
+
 echo "Process id of last command in background: $!"
