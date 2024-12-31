9bebdaceb3e3a7521ae2aee56d9cec0e8abf5dcb
vinaiuvr-joindevops <uvr.vinaikumar@gmail.com>
2024-12-31 17:08:24 +0530

    new file

---
+++ 08-special-variables.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "All variables passed :  $@ "
+echo "Number of variables :  $#"
+echo "Script name : $0 "
+echo "Present working directory: $PWD"
+echo "Home directory of current user:  $HOME"
+echo "Which user is running the script:  $USER"
+echo "Process Id of current script :  $$ "
+echo "Process id of last command in background: $!"
