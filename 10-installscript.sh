270a7eeeec5a9c6b2022db148a995b6e24ee1672
vinaiuvr-joindevops <uvr.vinaikumar@gmail.com>
2024-12-31 20:22:33 +0530

    new file

--- 10-installscript.sh
+++ 10-installscript.sh
@@ -2,9 +2,9 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then 
-    echo "ERROR: you must have sudo access to execute this script"
+    echo "ERROR :: you must have sudo access to execute this script"
     exit 1 #other than 0
 fi
 
