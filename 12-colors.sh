0ffac1c54997d1a0435d7a368720478e581cd497
Vinai <uvr.vinaikumar@gmail.com>
2025-01-02 19:39:20 +0530

    Update 12-colors.sh

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
-
-VALIDATE() {
-
-if [ $1 -ne 0 ]
+VALIDATE(){
+    if [ $1 -ne 0 ]
     then
-        echo -e "$2 ...$R FAILURE"
+        echo -e "$2 ... $R FAILURE"
         exit 1
     else
-        echo -e "$2 ...$G SUCCESS"
+        echo -e "$2 ... $G SUCCESS"
     fi
-
 }
 
-if [ $? -ne 0 ]
+if [ $USERID -ne 0 ]
 then
-    dnf install mysql -y
-
-    VALIDATE $? "Installing mysql"
-else
-    echo -e "MySQL is already ...$Y INSTALLED"
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo -e "MySQL is already ... $Y INSTALLED"
+fi
+
+
+dnf list installed git
 
 if [ $? -ne 0 ]
 then
     dnf install git -y
-    VALIDATE $? "Installing git"
-    
+    VALIDATE $? "Installing Git"
 else
-    echo -e "Git is already ...$Y INSTALLED"
+    echo -e "Git is already ... $Y INSTALLED"
 fi
